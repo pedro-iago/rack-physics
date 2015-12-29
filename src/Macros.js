3c52f78b8d2757243c1563244bf7c8ab31485493
Pedro Martins <pedro.m4rtins@gmail.com>
2015-12-29 04:23:14 -0300

    Macros
    Since I come from micro controllers programming, I also like to see all
    my constants defined in one place. This should give me a overview of
    what pieces I have (even though some others may be in a better place).
    Constants should probably be place here if they are recurrent in a lot
    of code.

---
+++ src/Macros.js
@@ -0,0 +1,27 @@
+export const BOX = 'box';
+export const SPHERE = 'sphere';
+export const CYLINDER = 'cylinder';
+
+export const JOINT_DISTANCE = 'jointDistance';
+export const JOINT_BALL_AND_SOCKET = "jointBallAndSocket";
+export const JOINT_HINGE = 'jointHinge';
+export const JOINT_WHEEL = 'jointWheel';
+export const JOINT_SLIDER = 'jointSlider';
+export const JOINT_PRISMATIC = 'jointPrismatic';
+
+export const BRUTE = 1;
+export const SWEEP = 2;
+export const TREE = 3;
+export const BROADPHASES = [BRUTE, SWEEP, TREE];
+
+export const SPAWN = 'SPAWN';
+export const INIT = 'INIT';
+export const SUBSCRIBE = 'SUBSCRIBE';
+export const STEP = 'STEP';
+export const HYDRATE = 'HYDRATE';
+export const TERMINATE = 'TERMINATE';
+
+export const BODIES = [BOX, SPHERE, CYLINDER];
+export const JOINTS = [JOINT_DISTANCE, JOINT_BALL_AND_SOCKET, JOINT_HINGE, JOINT_WHEEL, JOINT_SLIDER, JOINT_PRISMATIC];
+export const OBJECTS = [...BODIES, ...JOINTS];
+export const ACTIONS = [SPAWN, INIT, SUBSCRIBE, STEP, HYDRATE, TERMINATE];
