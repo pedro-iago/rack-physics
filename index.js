236d8dbe8f78ae7d6cc6230bd720ddf2384a2917
Pedro Martins <pedro.m4rtins@gmail.com>
2016-01-09 20:14:14 -0300

    fdfd

--- index.js
+++ index.js
@@ -11,41 +11,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// function render(){
-//   ReactDOM.render(
-//     <App />,
-//     document.getElementById("root")
-//   );
-//   requestAnimationFrame(render);
-// }
-// requestAnimationFrame(render);
-
-// //this is pretty interesting in that it makes the result dispatch to run in the requestAnimationFrame of react-three-renderer
-// let ReactUpdates = require('react/lib/ReactUpdates');
-// var fcfsBatchingStrategy = {
-//   isBatchingUpdates: true,
-//   batchedUpdates: function(callback, a, b, c, d, e) {
-//     callback(a, b, c, d, e);
-//     ReactUpdates.flushBatchedUpdates();
-//     //this is a possible place for requesting another three js frame
-//   }
-// };
-// ReactUpdates.injection.injectBatchingStrategy(fcfsBatchingStrategy);
-
-// let ReactUpdates = require('react/lib/ReactUpdates');
-// var rafBatchingStrategy = {
-//   isBatchingUpdates: true,
-//   batchedUpdates: function(callback, param) {
-//     callback(param);
-//   }
-// };
-// var tick = function() {
-//   ReactUpdates.flushBatchedUpdates();
-//   requestAnimationFrame(tick);
-// };
-// requestAnimationFrame(tick);
-// ReactUpdates.injection.injectBatchingStrategy(rafBatchingStrategy);
-
 window.onbeforeunload = () => ReactDOM.unmountComponentAtNode(
   document.getElementById("root")
 );
